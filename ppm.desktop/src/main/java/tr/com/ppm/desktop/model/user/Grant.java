f0ae46dfe768d84cd735e98206740fb8bf9c00d8
yoztas <yoztas@gmail.com>
2017-02-14 18:10:38 +0300

    DB2 ayarları

--- ppm.desktop/src/main/java/tr/com/ppm/desktop/model/user/Grant.java
+++ ppm.desktop/src/main/java/tr/com/ppm/desktop/model/user/Grant.java
@@ -2,9 +2,22 @@ package tr.com.ppm.desktop.model.user;
 
 import tr.com.ppm.desktop.model.common.AuditableEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 /**
  * @author Orhun Dalabasmaz
  */
+@Entity
 public class Grant extends AuditableEntity {
+	@Column
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
