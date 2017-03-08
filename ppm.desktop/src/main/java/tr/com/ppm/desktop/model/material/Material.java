2163c585c1ae990323ea463f04795a882aa5757d
yoztas <yoztas@gmail.com>
2017-03-08 20:08:53 +0300

    hammade ekranlarında ekle sil güncelle fonksiyonu geliştirmeleri

--- ppm.desktop/src/main/java/tr/com/ppm/desktop/model/material/Material.java
+++ ppm.desktop/src/main/java/tr/com/ppm/desktop/model/material/Material.java
@@ -1,12 +1,59 @@
 package tr.com.ppm.desktop.model.material;
 
 import tr.com.ppm.desktop.model.common.AuditableEntity;
+import tr.com.ppm.desktop.model.common.State;
+
+import javax.persistence.*;
 
 /**
  * @author Orhun Dalabasmaz
  */
+@Entity
+@Table(name = "MATERIAL")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Material extends AuditableEntity {
+
+	@Column(name = "CODE")
 	private String code;
+
+	@Column(name = "NAME")
 	private String name;
+
+	@Column(name = "DESCRIPTION")
 	private String description;
+
+	public Material(String code, String name, String description) {
+		this.code = code;
+		this.name = name;
+		this.description = description;
+	}
+
+	//for Hibernate
+	public Material() {
+
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- ppm.desktop/src/main/java/tr/com/ppm/desktop/model/material/RawMaterial.java
+++ ppm.desktop/src/main/java/tr/com/ppm/desktop/model/material/RawMaterial.java
@@ -1,7 +1,36 @@
 package tr.com.ppm.desktop.model.material;
 
+import tr.com.ppm.desktop.model.common.State;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Orhun Dalabasmaz
  */
+@Entity
+@Table(name = "RAW_MATERIAL")
 public class RawMaterial extends Material {
+
+	//for hibernate
+	public RawMaterial() {
+		super();
+	}
+
+	@Column(name = "STATE")
+	private State state;
+
+	public RawMaterial(String code, String name, String desc, State state) {
+		super(code, name, desc);
+		this.state = state;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
 }
