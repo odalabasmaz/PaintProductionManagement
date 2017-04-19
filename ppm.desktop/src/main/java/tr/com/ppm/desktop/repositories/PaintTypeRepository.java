4259b227b3e90e1517a4d7ee5e10528c22b0a65e
ykarabalkan <yusufkarabalkan@gmail.com>
2017-04-19 19:58:00 +0300

    PaintType Repository

---
+++ ppm.desktop/src/main/java/tr/com/ppm/desktop/repositories/PaintTypeRepository.java
@@ -0,0 +1,15 @@
+package tr.com.ppm.desktop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tr.com.ppm.desktop.model.material.PaintType;
+
+import java.util.List;
+
+/**
+ * @author ykarabalkan.
+ */
+@Repository
+public interface PaintTypeRepository extends JpaRepository<PaintType, Long> {
+	List<PaintType> findByNameContainingIgnoreCase(String name);
+}
