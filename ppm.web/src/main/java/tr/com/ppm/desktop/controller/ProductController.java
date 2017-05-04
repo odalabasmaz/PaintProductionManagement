23bed4c43106f384c54f8dc8470c2ad5ec692a70
utkuadk <umitadak@gmail.com>
2017-05-04 21:48:48 +0300

    login and product

--- ppm.web/src/main/java/tr/com/ppm/desktop/controller/ProductController.java
+++ ppm.web/src/main/java/tr/com/ppm/desktop/controller/ProductController.java
@@ -1,5 +1,6 @@
 package tr.com.ppm.desktop.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -27,15 +28,43 @@ public class ProductController {
 			path = "/rest/products")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Product> getProducts(@RequestParam(value = "name") String name) {
-		return service.list();
+		return StringUtils.isBlank(name) ? service.list() : service.findAllByName(name);
 	}
 
 	@RequestMapping(
 			method = RequestMethod.GET,
 			value = "/rest/products/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Customer getProduct(@PathVariable(value = "id") long id) {
-		return null;
+	public Product getProduct(@PathVariable(value = "id") long id) {
+		return service.findById(id);
 //		return service.findById(id);
 	}
+
+	@RequestMapping(
+			method = RequestMethod.POST,
+			path = "/rest/products")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody String addProduct(@RequestBody Product product) {
+		service.save(product);
+		return "{\"result\": \"Product saved!\"}";
+	}
+
+	@RequestMapping(
+			method = RequestMethod.PUT,
+			path = "/rest/products")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody String updateProduct(@RequestBody Product product) {
+		service.update(product);
+		return "{\"result\": \"Product updated!\"}";
+	}
+
+	@RequestMapping(
+			method = RequestMethod.DELETE,
+			path = "/rest/products")
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody String deleteProduct(@RequestParam(value = "id") long id) {
+//		service.deletePaintTypeById(id);
+		return "{\"result\": \"Product deleted!\"}";
+	}
+
 }
